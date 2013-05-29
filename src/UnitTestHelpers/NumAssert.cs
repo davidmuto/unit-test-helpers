b33721977470757090dde26c3a3922039e94084a
David Muto <david.muto@gmail.com>
2013-05-29 11:54:19 -0400

    Added NumAssert and ResourceLoader

---
+++ src/UnitTestHelpers/NumAssert.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestHelpers
+{
+    public class NumAssert
+    {
+        #region [LessThan]
+        
+        public static void IsLessThan(int referenceValue, int compareValue, string message = null)
+        {
+            IsLessThan((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsLessThan(float referenceValue, float compareValue, string message = null)
+        {
+            IsLessThan((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsLessThan(double referenceValue, double compareValue, string message = null)
+        {
+            if (compareValue >= referenceValue)
+            {
+                ThrowException(message);
+            }
+        }
+
+        #endregion
+
+        #region [LessThanOrEqual]
+
+        public static void IsLessThanOrEqual(int referenceValue, int compareValue, string message = null)
+        {
+            IsLessThanOrEqual((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsLessThanOrEqual(float referenceValue, float compareValue, string message = null)
+        {
+            IsLessThanOrEqual((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsLessThanOrEqual(double referenceValue, double compareValue, string message = null)
+        {
+            if (compareValue > referenceValue)
+            {
+                ThrowException(message);
+            }
+        }
+
+        #endregion
+
+        #region [GreaterThan]
+
+        public static void IsGreaterThan(int referenceValue, int compareValue, string message = null)
+        {
+            IsGreaterThan((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsGreaterThan(float referenceValue, float compareValue, string message = null)
+        {
+            IsGreaterThan((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsGreaterThan(double referenceValue, double compareValue, string message = null)
+        {
+            if (compareValue <= referenceValue)
+            {
+                ThrowException(message);
+            }
+        }
+
+        #endregion
+
+        #region [GreaterThan]
+
+        public static void IsGreaterThanOrEqual(int referenceValue, int compareValue, string message = null)
+        {
+            IsGreaterThanOrEqual((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsGreaterThanOrEqual(float referenceValue, float compareValue, string message = null)
+        {
+            IsGreaterThanOrEqual((double)referenceValue, (double)compareValue, message);
+        }
+
+        public static void IsGreaterThanOrEqual(double referenceValue, double compareValue, string message = null)
+        {
+            if (compareValue < referenceValue)
+            {
+                ThrowException(message);
+            }
+        }
+
+        private static void ThrowException(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                message = "IsLessThan Failed";
+            }
+
+            throw new AssertFailedException(message);
+        }
+
+        #endregion
+    }
+}
---
+++ src/UnitTestHelpers/ResourceLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestHelpers
+{
+    public class ResourceLoader
+    {
+        public static byte[] GetResource(string name)
+        {
+            var asm = Assembly.GetCallingAssembly();
+            var res = string.Format("{0}.{1}", asm.GetName().Name, name);
+
+            using (var stream = asm.GetManifestResourceStream(res))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public static string GetResourceString(string name)
+        {
+            var asm = Assembly.GetCallingAssembly();
+            var res = string.Format("{0}.{1}", asm.GetName().Name, name);
+
+            using (var stream = asm.GetManifestResourceStream(res))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
